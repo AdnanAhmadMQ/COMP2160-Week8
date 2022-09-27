aeec6f526eb4af81b0b0c0e2eb04feb7d4586049
AdnanAhmadMQ <69449019+AdnanAhmadMQ@users.noreply.github.com>
2022-09-27 20:11:36 +1000

    demo

--- COMP2160 Prac Week 8/Assets/Scripts/Score.cs
+++ COMP2160 Prac Week 8/Assets/Scripts/Score.cs
@@ -10,13 +10,14 @@ public class Score : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        ScoreKeeper.Instance.AddScore();
     }
 
     // Update is called once per frame
     void Update()
     {
         
-        //Debug.Log("score =" + ScoreKeeper.Instance.whatScore());
+        Debug.Log("score =" + ScoreKeeper.Instance.whatScore());
         scoreText.GetComponent<Text>().text = ("Score = " + ScoreKeeper.Instance.whatScore());
     }
 }
